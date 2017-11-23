9cb76d714bc2b902b2f72400eafd01f0fcc6cb65
Tom Brandon <tom.j.v.brandon@gmail.com>
2017-11-23 12:46:46 +0000

    Added tests for initialising with a set or variable capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,38 +2,35 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it "releases working bikes" do
+  it "Should release working bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to :see_bikes }
 
-  it "bike is docked" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq subject.see_bikes
-  end
-
-  it "bike is seen" do
+  it "Should see same bike when docked" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.see_bikes[-1]).to eq bike
   end
 
-  #it "Should raise error if try to dock bike when at full capacity" do
-    #  subject.dock(Bike.new)
-    #  expect { subject.dock(Bike.new) }.to raise_error("Docking station is full already")
-  #end
-
-  it { is_expected.to respond_to :see_bikes }
+  describe '#Initialize' do
+    it "Initializes with a variable capacity" do
+      station = DockingStation.new(50)
+      station.capacity.times {station.dock(Bike.new)}
+      expect { station.dock(Bike.new) }.to raise_error("Docking station is full already")
+    end
 
     it "set dock capacity when a new docking station is made" do
       expect(subject.capacity).to eq 20
     end
+  end
 
   describe '#release_bike' do
-    it "should release" do
+    it "should release bike from docking station" do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
@@ -48,6 +45,11 @@ describe DockingStation do
     it "Cannot dock more bikes than the default capacity in one docking station" do
       subject.capacity.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error("Docking station is full already")
+    end
+
+    it "Should dock bikes" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq subject.see_bikes
     end
   end
 end
